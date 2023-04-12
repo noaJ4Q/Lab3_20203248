22cddda5dd59137a4d86aa4f38f2aecab8381896
noe <noe.jaraq@gmail.com>
2023-04-11 20:41:36 -0500

    proyecto base

---
+++ src/main/java/com/example/lab3_20203248/Lab320203248Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203248;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320203248Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320203248Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20203248/Lab320203248ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203248;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320203248ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
